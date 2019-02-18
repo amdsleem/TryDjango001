26894b110022c3e46cfc507a1ed391e3d78cfae4
Amd Sleem <amdsleem@yahoo.com>
2019-02-18 14:20:44 +0200

    adding list display to admin

--- trydjango/posts/admin.py
+++ trydjango/posts/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "timestamp"]
     class Meta:
         model = Post
 
