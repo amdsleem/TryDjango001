2dfcc5821708c0fcc5254f7ce2a944e144d96ae8
Amd Sleem <amdsleem@yahoo.com>
2019-02-19 13:18:32 +0200

    for now I made a "CRUD" 4 paths

--- trydjango/posts/urls.py
+++ trydjango/posts/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 
 urlpatterns = [
-    path('', views.post_home, name='post_home'),
+    path('', views.post_list, name='post_list'),
+    path('create/', views.post_create, name='post_create'),
+    path('detail/', views.post_detail, name='post_detail'),
+    path('update/', views.post_update, name='post_update'),
+    path('delete/', views.post_delete, name='post_delete'),
 ]
